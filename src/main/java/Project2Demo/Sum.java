4674a9d59fd25a9e8119b68398470eb9fea09e8f
STUDENTLOANER <drisschima42@gmail.com>
2023-07-06 22:14:47 -0400

    this is 3 commit

---
+++ src/main/java/Project2Demo/Sum.java
@@ -0,0 +1,11 @@
+package Project2Demo;
+
+public class Sum {
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i=0;i<5;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
